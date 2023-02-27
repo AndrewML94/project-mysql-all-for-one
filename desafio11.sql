e8527fe54a5ad04f392bc4d047a5ec91801ca421
Andrew Lima <andrewmll94@gmail.com>
2023-02-27 15:59:00 -0300

    Conclusão do requisito 12 :rocket:

--- desafio11.sql
+++ desafio11.sql
@@ -1,2 +1,2 @@
-SELECT notes FROM northwind.purchase_orders
+SELECT notes FROM purchase_orders
 WHERE notes >= 'Purchase generated based on Order #30' AND notes <= 'Purchase generated based on Order #39';
